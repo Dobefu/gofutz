dea4119e679ea96699655d7038da0d02c2f9c8c2
Connor van Spronssen <info@dobefu.com>
2025-08-27 18:01:08 +0200

    Initial commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/Dobefu/gofutz
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,11 @@
+// Package main provides the main entrypoint of the application.
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	fmt.Fprintln(os.Stdout, "TODO")
+}
