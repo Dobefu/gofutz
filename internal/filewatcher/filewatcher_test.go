1509d5cd23bf685180531c5c8f3e5ee846422af8
Connor van Spronssen <info@dobefu.com>
2025-08-31 15:16:16 +0200

    test: Add a test for the CollectAllTestFiles function

---
+++ internal/filewatcher/filewatcher_test.go
@@ -0,0 +1,31 @@
+package filewatcher
+
+import "testing"
+
+func TestCollectAllTestFiles(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name string
+	}{
+		{
+			name: "regular test files",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			files, err := CollectAllTestFiles()
+
+			if err != nil {
+				t.Fatalf("expected no error, got: %s", err.Error())
+			}
+
+			if len(files) == 0 {
+				t.Fatalf("expected files, got none")
+			}
+		})
+	}
+}
