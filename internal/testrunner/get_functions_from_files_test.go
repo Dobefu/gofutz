1cb48bf1c48b5a39a913741e0fda4163c153010d
Connor van Spronssen <info@dobefu.com>
2025-09-05 11:26:10 +0200

    test: Add tests for the GetFunctionsFromFiles method

--- internal/testrunner/get_functions_from_files_test.go
+++ internal/testrunner/get_functions_from_files_test.go
@@ -7,7 +7,7 @@ import (
 	"testing"
 )
 
-func TestGetFunctionsFromFile(t *testing.T) {
+func TestGetFunctionsFromFiles(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
@@ -28,66 +28,56 @@ func TestGetFunctionsFromFile(t *testing.T) {
 				t.Fatalf("expected no error, got: %s", err.Error())
 			}
 
-			functions, _, err := GetFunctionsFromFile(
-				filepath.Join(cwd, "get_functions_from_file.go"),
+			functions, err := GetFunctionsFromFiles(
+				[]string{filepath.Join(cwd, "get_functions_from_files.go")},
 			)
 
 			if err != nil {
 				t.Fatalf("expected no error, got: %s", err.Error())
 			}
 
-			if len(functions) <= 0 {
+			if len(functions) == 0 {
 				t.Fatalf("expected functions, got none")
 			}
 		})
 	}
 }
 
-func TestGetFunctionsFromFileErr(t *testing.T) {
+func TestGetFunctionsFromFilesErr(t *testing.T) {
 	t.Parallel()
 
 	tests := []struct {
 		name     string
-		file     string
+		files    []string
 		expected string
 	}{
 		{
-			name: "nonexistent file",
-			file: os.DevNull,
+			name:  "nonexistent file",
+			files: []string{os.DevNull},
 			expected: fmt.Sprintf(
 				"%s:1:1: expected 'package', found 'EOF'",
 				os.DevNull,
 			),
-		},
-		{
-			name:     "empty filename",
-			file:     "",
-			expected: "file is empty",
-		},
-		{
-			name:     "error reading file",
-			file:     "/bogus",
-			expected: "open /bogus: no such file or directory",
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
 			t.Parallel()
+
+			_, err := GetFunctionsFromFiles(test.files)
+
+			if err == nil {
+				t.Fatalf("expected error, got nil")
+			}
+
+			if err.Error() != test.expected {
+				t.Fatalf(
+					"expected error to be \"%s\", got: \"%s\"",
+					test.expected,
+					err.Error(),
+				)
+			}
 		})
-
-		_, _, err := GetFunctionsFromFile(test.file)
-
-		if err == nil {
-			t.Fatalf("expected error, got nil")
-		}
-
-		if err.Error() != test.expected {
-			t.Fatalf(
-				"expected error to be \"%s\", got: \"%s\"",
-				test.expected,
-				err.Error(),
-			)
-		}
 	}
 }
