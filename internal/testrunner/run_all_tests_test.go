47e4b3a70baf9f8bb225b77f6797bea59116c2af
Connor van Spronssen <info@dobefu.com>
2025-09-06 10:12:38 +0200

    test: Add a test for the RunAllTests method

---
+++ internal/testrunner/run_all_tests_test.go
@@ -0,0 +1,66 @@
+package testrunner
+
+import (
+	"os"
+	"testing"
+)
+
+func TestRunAllTests(t *testing.T) {
+	t.Parallel()
+
+	cwd, err := os.Getwd()
+
+	if err != nil {
+		t.Fatalf("could not get current working directory: %s", err.Error())
+	}
+
+	tests := []struct {
+		name     string
+		files    map[string]File
+		expected []string
+	}{
+		{
+			name:     "no test files",
+			files:    map[string]File{},
+			expected: []string{},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			runner := &TestRunner{ // nolint:exhaustruct
+				files: test.files,
+			}
+
+			fullOutput := []string{}
+
+			runner.RunAllTests(func(_ File) error {
+				return nil
+			}, func(output string) error {
+				fullOutput = append(fullOutput, output)
+
+				return nil
+			}, func() {})
+
+			if len(fullOutput) != len(test.expected) {
+				t.Fatalf(
+					"expected %d output lines, got: %d",
+					len(test.expected),
+					len(fullOutput),
+				)
+			}
+
+			for i := range fullOutput {
+				if fullOutput[i] != test.expected[i] {
+					t.Fatalf(
+						"expected output to be %s, got: %s",
+						test.expected[i],
+						fullOutput[i],
+					)
+				}
+			}
+		})
+	}
+}
