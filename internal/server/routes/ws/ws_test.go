67ecc456d44657b5605cf0b9135ced1185f69192
Connor van Spronssen <info@dobefu.com>
2025-09-07 12:37:27 +0200

    test: Add additional tests for the ws route

--- internal/server/routes/ws/ws_test.go
+++ internal/server/routes/ws/ws_test.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/Dobefu/gofutz/internal/websocket"
 	gorillawebsocket "github.com/gorilla/websocket"
@@ -40,6 +41,8 @@ func TestHandle(t *testing.T) {
 
 			defer func() { _ = ws.Close() }()
 
+			time.Sleep(100 * time.Millisecond)
+
 			initMessage := websocket.UpdateMessage{
 				Method: "gofutz:init",
 				Error:  "",
@@ -60,6 +63,44 @@ func TestHandle(t *testing.T) {
 
 			if err != nil {
 				t.Fatalf("could not send message: %v", err)
+			}
+		})
+	}
+}
+
+func TestHandleErr(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		setup    func(*http.Request)
+		expected int
+	}{
+		{
+			name: "invalid headers",
+			setup: func(req *http.Request) {
+				req.Header.Set("Upgrade", "websocket")
+			},
+			expected: http.StatusBadRequest,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			req := httptest.NewRequest("GET", "/ws", nil)
+			test.setup(req)
+			w := httptest.NewRecorder()
+
+			Handle(w, req)
+
+			if w.Code != test.expected {
+				t.Fatalf(
+					"expected status code to be %d, got %d",
+					test.expected,
+					w.Code,
+				)
 			}
 		})
 	}
